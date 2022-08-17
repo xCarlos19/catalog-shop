9f97c2a36fd9e85d5b2bfef1e427dfdcaa5a1b65
Juan Carlos <Juank199819@gmail.com>
2022-08-17 17:00:40 +0200

    Checkstyle problems

--- CATALOG.boot/src/main/java/catalog/ShopApplication.java
+++ CATALOG.boot/src/main/java/catalog/ShopApplication.java
@@ -5,8 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ShopApplication {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         SpringApplication.run(ShopApplication.class);
     }
+
 
 }
