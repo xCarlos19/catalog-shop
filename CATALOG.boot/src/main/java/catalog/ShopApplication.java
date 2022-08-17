7cd44117e5bac0ae8c16304dbe5673026742ce7a
Juan Carlos <Juank199819@gmail.com>
2022-08-17 16:48:41 +0200

    POM changes

--- CATALOG.boot/src/main/java/catalog/ShopApplication.java
+++ CATALOG.boot/src/main/java/catalog/ShopApplication.java
@@ -5,9 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ShopApplication {
-    public static void main(String[] args) {
+    public static void main(String[] args){
         SpringApplication.run(ShopApplication.class);
-
     }
 
 }
